cdc1a3c9fdca66fdc9db2a72f525d87f93c032c2
https://github.com/IbatullinaMaria <mpbugaga@yandex.ru>
2022-09-15 13:05:06 +0300

    add rootReducer

---
+++ src/store/customerReducer.js
@@ -0,0 +1,15 @@
+const defaultState = {
+    cash: 0,
+};
+
+export const customerReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case 'ADD_CUSTOMER':
+            return { ...state, cash: state.cash + action.payload };
+        case 'GET_CUSTOMER':
+            return { ...state, cash: state.cash - action.payload };
+
+        default:
+            return state;
+    }
+};
